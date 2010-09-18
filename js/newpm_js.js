797244431eca87e352348861119df7d8650b2047
Pete Milkman <peter_milkman@hotmail.com>
2010-09-17 18:43:04 -0700

    Added a couple comments, spaced some things out.

--- js/newpm_js.js
+++ js/newpm_js.js
@@ -4,6 +4,9 @@ var fC, fS, fromDot, toDot, mouseX = 0,
     mouseY = 0;
 var Pi2 = Math.PI * 2;
 var fakeLimit = Pi2 * 100 >> 0;
+
+
+// fS and fC are cosine and sine lookup table arrays since sin and cos are expensive math ops we 'f'ake it.
 fS = [];
 for (var i = -fakeLimit; i < fakeLimit; i++) {
     fS[i] = Math.sin(i * 0.01);
@@ -72,7 +75,8 @@ function draw() {
     canvas.setAttribute('height', height);
 
     ctx.restore();
-    (aC > fakeLimit)?aC = 1:++aC; //sanity check
+    aC++;
+    if (aC > fakeLimit) aC = 1; //sanity check
     // draw sky
     ctx.fillStyle = bggradsky;
     ctx.fillRect(0, 0, canvas.width, horizon);
@@ -91,44 +95,70 @@ function draw() {
     var getsky = ctx.getImageData(0, 0, canvas.width, horizon),
         gsD = getsky.data,
         // Everything above the sky, used as source data to generate the reflection
+
         gsW = getsky.width,
         // what's the width? sky is 2x big on purpose, so that edges don't wrap in reflection
+
         gsH = getsky.height,
         // simple height.
+
         rS = getsky.width << 2,
         // 4 values per pixel in canvas, <<2 is a far faster version of *4.
+
         startRow = gsH >> 1,
         // since the water portion is half the height of the sky, we can start half through getsky. >>1 = faster /2
+
         rCA = 50,
         // rCA is an accumulator that is used to have waves be closer together near the horizon
+
         rCAInc = 0.95,
         // rCA is multiplied by this each row in the main loop
+
         aCDiv4H = aC << 3,
         // aC gets incremented each frame of animation. this is the value that moves the waves toward the viewer
+
         aCDiv4V = aC << 3,
         // this value moves the waves left each frame.
+
         cC = gsW >> 1; // Stored alias for the middle of the image data.
+
     for (var i = gsH; i >= startRow; i--) {
+
         rCA = rCA * rCAInc; // see the rCA comments above this loop
+
         cosVal = (i * rCA) + aCDiv4H; // The actual value that gets Cosine'd for the waves horizontally.
+
         var iH = gsH - i - 2,
             // since we iterate backwards, this alias helps with the math
+
             wH = iH << 2,
             // Wave height. explained later.
+
             cosRow = (fC[~~ (cosVal % fakeLimit)] * wH) >> 2,
             // (this is + or -) instead of pulling row 'i'. Add cosRow to 'i' and that's your row from sky.
+
             rowCosMath = (i + cosRow) * rS,
             // adding to 'i' and multiply by the rowsize to get starting point for the inner loop.
+
             toDot = iH * rS + (lRC << 2),
             // pre-calc'ing the leftmost .data position of this row in the destination .data
+
             sinPreCalc = rCA - (rCA >> 4); // rCA perspective math doesn't change inside the inner loop, so it's extracted here and aliased.
+
         for (var j = lRC; j < rRC; j++) {
+
             sinVal = ((j - cC) * sinPreCalc + aCDiv4V); // The value to do Sine against 
+
             alti = (fS[~~ (sinVal % fakeLimit)] * wH) >> 2; // Sine sinVal, multiply it by the wave height, then divide by 4 (>>2)
+
             fromDot = rowCosMath + ((j + alti) << 2); // Final math to determine where to pull the source dot values from
+
             makewater.data[toDot++] = gsD[fromDot++]; // Do the actual value transfers here (red)
+
             makewater.data[toDot++] = gsD[fromDot++]; // and here (green)
+
             makewater.data[toDot++] = gsD[fromDot++]; // and here (blue)
+
             makewater.data[toDot++] = 255; // opacity is always opaque, so just use literal 255.
         }
     }
